2411cc5d36f5cd36cf63a9188444d97f366d62cc
mylenaverspeelt <mylena49@hotmail.com>
2023-12-06 11:29:14 -0300

    creating routes

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http"
 
 const PORT = 3000
 
+const routs = {
+    "/" : "Home Page",
+    "/authors" : "Authors Page",
+    "/books" : "Books Page"
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"})
-    res.end("Curso de Node.js")
+    res.end(routs[req.url])
 })
 
 
