81405c4e0e65ebf973309af8389e611ae6322f7d
mylenaverspeelt <mylena49@hotmail.com>
2023-12-07 10:54:52 -0300

    add put method

--- server.js
+++ server.js
@@ -1,27 +1,45 @@
-import express from "express";
+import express from "express"
 
 const PORT = 3000
 
-const app = express();
+const app = express()
 
 //middleware: faz o parse das requisições pra json pois vem em string
 app.use(express.json())
 
-const books = [{ id: 1, title: "Hunguer Games" }, { id: 2, title: "Harry Potter" }]
+const booksList = [{ id: 1, title: "Hunguer Games" }, { id: 2, title: "Harry Potter" }]
 
 app.get("/", (req, res) => {
     res.status(200).send("Home")
 })
 
 app.get("/books", (req, res) => {
-    res.status(200).json(books)
+    res.status(200).json(booksList)
 })
 
 app.post("/books", (req, res) => {
-    books.push(req.body)
+    booksList.push(req.body)
     res.status(201).send("Livro cadastrado com sucesso!")
 })
 
+function findBookInArray(id) {
+    let index = booksList.findIndex(item => item.id === Number(id))
+    return index
+}
+
+// : é pra indicar uma variavel
+app.get("/books/:id", (req, res) => {
+    let indexBook = findBookInArray(req.params.id)
+    res.status(200).json(booksList[indexBook])
+})
+
+app.put("/books/:id", (req, res)=>{
+    let indexBook = findBookInArray(req.params.id)
+    booksList[indexBook].title = req.body.title
+    res.status(200).json(booksList)
+})
+
+
 app.listen(PORT, () => {
-    console.log("Escutando servidor na porta", 3000);
+    console.log("Escutando servidor na porta", 3000)
 })
