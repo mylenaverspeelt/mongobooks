e86d8984910beefd9e9fe04a4bac14eeb8019e9e
mylenaverspeelt <mylena49@hotmail.com>
2023-12-07 10:17:53 -0300

    add express, get and post methods

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
-import http from "http"
+import express from "express";
 
 const PORT = 3000
 
-const routs = {
-    "/" : "Home Page",
-    "/authors" : "Authors Page",
-    "/books" : "Books Page"
-}
+const app = express();
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"})
-    res.end(routs[req.url])
+//middleware: faz o parse das requisições pra json pois vem em string
+app.use(express.json())
+
+const books = [{ id: 1, title: "Hunguer Games" }, { id: 2, title: "Harry Potter" }]
+
+app.get("/", (req, res) => {
+    res.status(200).send("Home")
 })
 
+app.get("/books", (req, res) => {
+    res.status(200).json(books)
+})
 
-server.listen(PORT, ()=>{
+app.post("/books", (req, res) => {
+    books.push(req.body)
+    res.status(201).send("Livro cadastrado com sucesso!")
+})
+
+app.listen(PORT, () => {
     console.log("Escutando servidor na porta", 3000);
 })
