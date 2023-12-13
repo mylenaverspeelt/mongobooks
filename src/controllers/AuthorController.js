723eb3c8e76af9e833f83f277aeda9c680dd7331
mylenaverspeelt <mylena49@hotmail.com>
2023-12-13 10:21:36 -0300

    refactor error handling foundAuthorById

--- src/controllers/AuthorController.js
+++ src/controllers/AuthorController.js
@@ -1,4 +1,5 @@
-import {authorModel} from "../models/Author.js"
+import mongoose from "mongoose"
+import { authorModel } from "../models/Author.js"
 
 //classe com todos os metodos de req/res referentes a entidade author
 
@@ -17,10 +18,20 @@ class AuthorController {
     try {
       const id = req.params.id
       const author = await authorModel.findById(id)
-      res.status(200).json(author)
+
+      if (author !== null) {
+        res.status(200).json(author)
+      } else {
+        res.status(404).send("Id do Autor não localizado")
+      }
 
     } catch (error) {
-      res.status(500).json({ message: `${error.message} - falha ao buscar autor específico` })
+      //quando ha um caractere diferente do esperado pelo schema
+      if (error instanceof mongoose.Error.CastError) {
+        res.status(400).json({ message: `${error.message} - Algum dos dados fornecidos estão incorretos` })
+      } else {
+        res.status(500).json({ message: `${error.message} - erro interno do servidor` })
+      }
     }
   }
 
@@ -39,7 +50,7 @@ class AuthorController {
   static async addNewAuthor(req, res) {
     try {
       const newAuthor = await authorModel.create(req.body)
-      res.status(201).json({ message: "autor criado com sucesso!", author: newAuthor})
+      res.status(201).json({ message: "autor criado com sucesso!", author: newAuthor })
     } catch (error) {
       res.status(500).json({ message: `${error.message} - falha ao cadastrar autor` })
     }
